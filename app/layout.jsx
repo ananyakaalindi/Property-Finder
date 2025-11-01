4589fe637e08491f36c81e3fb15f2bc2c1b39637
Ananya M <89152019+ananyakaalindi@users.noreply.github.com>
2025-11-01 23:16:21 +0530

    Add files via upload

---
+++ app/layout.jsx
@@ -0,0 +1,35 @@
+import Footer from '@/components/Footer';
+import '../assets/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import { GlobalProvider } from '@/context/GlobalContext';
+import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css';
+
+export const metadata = {
+  title: 'Property Finder',
+  keywords: 'rental, property, real estate',
+  description: 'Find the perfect rental property'
+}
+
+const MainLayout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <GlobalProvider>
+        <html>
+          <body>
+            <Navbar />
+            <main>
+              {children}
+            </main>
+            <Footer />
+            <ToastContainer />
+          </body>
+        </html>
+      </GlobalProvider>
+    </AuthProvider>
+  )
+}
+
+export default MainLayout
